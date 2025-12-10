032e1c880d541c324ca1827737a3afc3efbca49b
Dukeward <dukeward@Edwards-MacBook-Pro.local>
2025-12-10 08:40:21 -0500

    Cleanup

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC5MTLwFCWQXm_uw_0ALTCsgCeB0z_o56Q",
-    authDomain: "theforkofit.firebaseapp.com",
-    projectId: "theforkofit",
-    storageBucket: "theforkofit.appspot.com",
-    messagingSenderId: "981864557134",
-    appId: "1:981864557134:web:702dbc62de2086c458b11f",
-    measurementId: "G-E3T0QXDNXL"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
